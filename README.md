c5f858b73e84ca93c501e54f43ee12aa4fa674a5
Giselle Raposo <raposo_gigi@icloud.com>
2021-03-06 23:24:50 -0500

    645788735432356758598 hours later beginner still struggling 1436466 billion %

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # timeBlock
 A simple calendar app to schedule your work day.
+
+Start managing you time, not just your task. Use the time blocking method to divide your day into blocks of time.
+
--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,71 @@
 var todayDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 $('#currentDay').text(todayDate)
-	
 
-$('.saveBtn').on('click', function(){
+/*$(function(){
+  
+    loaddata(); //Load the saved data
+    // function loadData
+    function loaddata() {
+        $('.textarea').html(localStorage.mydata);
+    }
+*/
 
-    $('input[type="text"]').each(function(){    
-        var id = $(this).attr('id');
-        var value = $(this).val();
-       localStorage.setItem(id, value);
 
-    });   
-});
 
+//save to local storage
+//$(document).ready(function() {
+
+//});	
+
+//$('.saveBtn').on('click', function(){
+
+   // $('input[type="text"]').each(function(){    
+        //var id = $(this).attr('id');
+        //var value = $(this).val();
+       //localStorage.setItem(id, value);
+
+   // });   
+//});
+// Get data hour value
+//moment().hour(Number); //
+
+var eventList = localStorage.getItem('savedEvents');
+
+// for (let i = 0; i < eventList.length; i++) {
+//     console.log(eventList[i]);
+// }
+
+if (eventList == null) {
+    console.log("NO EVENTS IN MEMORY");
+    var savedEvents = [];
+} else {
+    var savedEvents = [];
+
+    // push each new DESCRIPTION
+    // as a STRING ELEMENT to the savedEvents
+    // ARRAY
+
+    // find a way
+    // to iterate through this array and
+    // set the VALUES of each TIMEBLOCK
+    savedEvents.push(eventList);
+}
+
+console.log(savedEvents);
+
+// We need to create a function that will
+// be called whenever we click a "Save" button
+
+// This function will CREATE an event
+function addEvent(hour) {
+    var description = document.getElementById(`data-hour-${hour}`).value;
+
+    var event = description;
+
+    savedEvents.push(event);
+
+    localStorage.setItem('savedEvents', savedEvents);
+}
 
 
 
