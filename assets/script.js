ee437c98434a5ab979f931da57579718e6b1050d
Giselle Raposo <raposo_gigi@icloud.com>
2021-03-06 20:04:57 -0500

    updated HTML, CSS. JS GRRRRRR

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,16 @@
-$(document).ready(function () {
-    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+var todayDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+$('#currentDay').text(todayDate)
+	
 
+$('.saveBtn').on('click', function(){
+
+    $('input[type="text"]').each(function(){    
+        var id = $(this).attr('id');
+        var value = $(this).val();
+       localStorage.setItem(id, value);
+
+    });   
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
